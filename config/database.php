75c0fa57509ef19382eff5eaae565dcad696fdab
LeTBichPhuong <phuongbon1524@gmail.com>
2025-11-06 01:31:57 +0700

    Fix PostgreSQL SSL connection for Render

--- config/database.php
+++ config/database.php
@@ -112,21 +112,19 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'require'),
-            'options' => extension_loaded('pdo_pgsql') ? [
-                PDO::ATTR_TIMEOUT => 5,
-            ] : [],
+            'sslmode' => env('DB_SSLMODE', 'require'), // ✅ Thêm dòng này
         ],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DB_URL'),
