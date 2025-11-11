304c8bcd970e572de84587a0116d94f860017521
LeTBichPhuong <phuongbon1524@gmail.com>
2025-11-11 08:30:33 +0700

    update

--- resources/views/Product.blade.php
+++ resources/views/Product.blade.php
@@ -137,6 +137,6 @@
 
     <script src="{{ asset('JS/Product.js') }}"></script>
     <script src="{{ asset('JS/script.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> --}}
 </body>
 </html>
